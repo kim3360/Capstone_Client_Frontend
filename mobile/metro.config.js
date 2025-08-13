263f9eb4a03bae299a259252d548e27e61c079a3
KimTaeWoo <70637743+kim3360@users.noreply.github.com>
2025-08-13 17:56:34 +0900

    Fix/#34 (#39)

--- mobile/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("@expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = {
-  ...config,
-  projectRoot: __dirname, // 👈 루트 경로를 명시적으로 지정
-  watchFolders: [__dirname + '/../package'], // 모노레포 하위 의존성 watch 설정
-};
--- mobile/src/services/AuthContext.tsx
+++
@@ -1,8 +0,0 @@
-// AuthContext.tsx
-import React from 'react';
-
-export const AuthContext = React.createContext({
-    isLoggedIn: false,
-    setIsLoggedIn: (value: boolean) => {},
-  });
-  
